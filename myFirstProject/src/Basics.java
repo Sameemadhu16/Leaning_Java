991fcc2918679b94d27e0a5d9126a0987d3d238d
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-06-04 08:48:20 +0530

    basics like comments and print function

---
+++ myFirstProject/src/Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+    public static void main(String[] args){
+
+        System.out.println("I love pizza");
+        System.out.println("Its really good!\n");
+
+        System.out.print("\"I love pizza\"");
+        System.out.print("\tIts really good!");
+        
+        //This is a single line comment
+
+        /*
+         * this 
+         * is 
+         * a 
+         * multi line 
+         * comment
+         */
+    }
+    
+}
