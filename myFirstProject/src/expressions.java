4978fbf8fe115ce799af82fe930aabc18de94b2a
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-06-05 13:13:33 +0530

    variables

---
+++ myFirstProject/src/expressions.java
@@ -0,0 +1,20 @@
+public class expressions {
+    public static void main(String[] args){
+
+        // expressions = operands & operators
+        // operands = values, varie=ables, numbers, quantity
+        // operators = + - / %
+
+        int friends = 10;
+        friends = friends % 3;
+        System.out.println(friends);
+
+        double length = 100;
+        length = (double)length / 3;
+        System.out.println(length);
+
+    }
+    
+}
+ 
+
